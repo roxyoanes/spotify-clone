83be6f63fbb275d183348eeff2ff6923a49693bb
roxyoanes <roxioanes11@gmail.com>
2020-06-19 12:24:37 +0200

    Profile api call

---
+++ pages/profile.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+
+import { spotifyApi } from "../src/server/spotifyApi";
+
+const Profile: NextPage<any> = (props) => {
+  console.log(props);
+  return <div>{JSON.stringify(props)}</div>;
+};
+
+Profile.getInitialProps = async () => {
+  const profileData = await spotifyApi.getMe();
+
+  return profileData;
+};
+
+export default Profile;
