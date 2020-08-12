a489282407328d2fa79e5066e46eb9256de9f907
roxyoanes <roxioanes11@gmail.com>
2020-08-12 11:55:25 +0200

    Style burger menu in playlist

--- src/Playlist.tsx
+++ src/Playlist.tsx
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router";
+import Router from "next/router";
+
 import { NextPage } from "next";
 import { IPlaylist } from "./types";
 
 import StyledPlaylist from "./styles/playlist";
+import { useState } from "react";
 
 interface IProps {
   playlist: IPlaylist;
@@ -11,6 +14,13 @@ interface IProps {
 const Playlist: NextPage<IProps> = ({ playlist }) => {
   const router = useRouter();
   const { playlistId } = router.query;
+
+  const [openPlaylistMenu, setOpenPlaylistMenu] = useState(false);
+
+  const togglePlaylistMenu = () => {
+    setOpenPlaylistMenu(!openPlaylistMenu);
+  };
+
   return (
     <StyledPlaylist>
       <div className="container">
@@ -35,28 +45,55 @@ const Playlist: NextPage<IProps> = ({ playlist }) => {
                 alt="play-icon"
               ></img>
             </button>
-            <button className="playlist-menu-btn">
-              <img
-                className="playlist-menu-img"
-                src="/ellipsis-h-solid.svg"
-                alt="play-icon"
-              ></img>
-            </button>
+            <div className="playlist-menu-container">
+              <button
+                className="playlist-menu-btn"
+                onClick={togglePlaylistMenu}
+              >
+                <img
+                  className="playlist-menu-img"
+                  src="/ellipsis-h-solid.svg"
+                  alt="play-icon"
+                ></img>
+              </button>
+              {openPlaylistMenu ? (
+                <div className="playlist-menu-list">
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "baseline",
+                      padding: "0",
+                      margin: "0",
+                    }}
+                  >
+                    <button className="menu-btn">Start Radio</button>
+                    <button className="menu-btn">Make secret</button>
+                    <button className="menu-btn">Delete</button>
+                    <button className="menu-btn">Copy Playlist link</button>
+                    <button className="menu-btn">Open in Desktop app</button>
+                  </div>
+                </div>
+              ) : null}
+            </div>
           </div>
           {playlist.tracks.items.map((item) => (
             <div className="song-container" key={item.id}>
+              {console.log("cc", item)}
               <img
                 className="music-icon"
                 src="/music-solid.svg"
                 alt="music-icon"
               ></img>
-              <p className="song-title">{item.track.name}</p>
-              <div className="artists-container">
-                {item.track.artists.map((artist) => (
-                  <p className="artist" key={item.id}>
-                    {artist.name}
-                  </p>
-                ))}
+              <div className="song-wrapper">
+                <p className="song-title">{item.track.name}</p>
+                <div className="artists-container">
+                  {item.track.artists.map((artist) => (
+                    <p className="artist" key={item.id}>
+                      {artist.name}
+                    </p>
+                  ))}
+                </div>
               </div>
             </div>
           ))}
--- src/styles/playlist.ts
+++ src/styles/playlist.ts
@@ -29,6 +29,7 @@ const StyledPlaylist = styled.div`
   }
   .buttons-container {
     display: flex;
+    align-items: center;
   }
   .play-btn {
     padding: 15px 20px;
@@ -51,11 +52,29 @@ const StyledPlaylist = styled.div`
   .playlist-menu-img {
     width: 25px;
   }
+  .playlist-menu-list {
+    position: absolute;
+    background-color: #131413;
+    padding: 15px;
+    border-radius: 8px;
+  }
+  .menu-btn {
+    background-color: transparent;
+    color: #b3b3b3;
+    padding: 10px 0;
+    font-weight: 300;
+    border: none;
+  }
   .song-container {
     padding: 15px 0;
+    display: grid;
+    grid-template-columns: 20px 1fr;
   }
   .music-icon {
     width: 15px;
+  }
+  .song-wrapper {
+    margin-left: 10px;
   }
   .song-title {
     font-size: 16px;
