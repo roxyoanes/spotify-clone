164cbc74204f49fea0ab8c6edcb0583e4082b938
roxyoanes <roxioanes11@gmail.com>
2020-08-18 11:27:17 +0200

    Add album page

---
+++ pages/album/[albumId].tsx
@@ -0,0 +1,77 @@
+import styled from "@emotion/styled";
+
+import { IPlaylist, IProfileData, IAlbum } from "../../src/types";
+import { NextPage } from "next";
+import toggleSidebarHook from "../../src/toggleSidebarHook";
+import Sidebar from "../../src/Sidebar";
+import Navbar from "../../src/Navbar";
+import { withRedux, IReduxStoreProps } from "../../src/redux/redux";
+import { Global, css } from "@emotion/core";
+import Album from "../../src/AlbumCard";
+
+interface IStyledProps {
+  opensidebar: boolean;
+}
+interface IProps {
+  newReleases: IAlbum[];
+  profileData: IProfileData;
+}
+
+const StyledContainer = styled.div<IStyledProps>`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: ${({ opensidebar }) =>
+    opensidebar === true ? "1fr 4fr" : "1fr"};
+`;
+
+const StyledRightSideContainer = styled.div<IStyledProps>`
+  display: grid;
+  background: linear-gradient(#212121, #131413);
+`;
+
+const AlbumId: NextPage<IProps> = ({ profileData, newReleases }) => {
+  const { toggleSidebar, openSidebar } = toggleSidebarHook();
+
+  return (
+    <StyledContainer opensidebar={openSidebar}>
+      <Sidebar openSidebar={openSidebar} />
+      <StyledRightSideContainer opensidebar={openSidebar}>
+        <Navbar
+          openSidebar={openSidebar}
+          toggleSidebar={toggleSidebar}
+          profileData={profileData}
+        />
+        <Album newReleases={newReleases} />
+        <Global
+          styles={css`
+            html,
+            body {
+              padding: 0;
+              margin: 0 !important;
+              font-family: "arial", sans-serif;
+            }
+            a {
+              text-decoration: none;
+              cursor: pointer;
+              color: #b3b3b3;
+              &:hover {
+                color: #fff;
+              }
+            }
+          `}
+        />
+      </StyledRightSideContainer>
+    </StyledContainer>
+  );
+};
+
+AlbumId.getInitialProps = async ({ reduxStore }: IReduxStoreProps) => {
+  /*  const store = reduxStore.getState();
+  console.log("st", store); */
+  /* 
+  return {
+    newReleases: store.newreleases,
+  }; */
+};
+export default withRedux(AlbumId);
