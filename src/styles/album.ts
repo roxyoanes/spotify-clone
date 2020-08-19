93a5183ce1c7f1bea6b26707c38362a36a67d209
roxyoanes <roxioanes11@gmail.com>
2020-08-19 11:48:52 +0200

    Add style component for album

---
+++ src/styles/album.ts
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+const StyledAlbum = styled.div`
+  color: white;
+  padding: 20px 30px;
+  margin: 0 40px;
+
+  .album-main {
+    display: flex;
+    align-items: end;
+    padding: 40px 30px;
+  }
+  .album-pic {
+    height: 260px;
+  }
+  .text-card {
+    margin-left: 30px;
+  }
+  .album-type {
+    font-size: 24px;
+    margin: 20px 15px;
+  }
+  .name {
+    font-size: 80px;
+    margin: 0;
+  }
+  .info-artist {
+    color: #b3b3b3;
+    font-size: 14px;
+    margin: 10px 0;
+  }
+`;
+
+export default StyledAlbum;
