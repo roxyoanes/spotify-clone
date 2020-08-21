f049f60f019f478a13990c8b4e1b913d5b130e9f
roxyoanes <roxioanes11@gmail.com>
2020-08-21 16:28:29 +0200

    Change name page

---
+++ pages/collection/playlists.tsx
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+
+import { IProfileData } from "../../src/types";
+import { NextPage } from "next";
+import toggleSidebarHook from "../../src/toggleSidebarHook";
+import Sidebar from "../../src/Sidebar";
+import Navbar from "../../src/Navbar";
+import { withRedux, IReduxStoreProps } from "../../src/redux/redux";
+import { Global, css } from "@emotion/core";
+import Library from "../../src/Library";
+import { spotifyApi } from "../../src/server/spotifyApi";
+
+interface IStyledProps {
+  opensidebar: boolean;
+}
+interface IProps {
+  profileData: IProfileData;
+}
+
+const StyledContainer = styled.div<IStyledProps>`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: ${({ opensidebar }) =>
+    opensidebar === true ? "1fr 4fr" : "1fr"};
+`;
+
+const StyledRightSideContainer = styled.div<IStyledProps>`
+  background: linear-gradient(#212121, #131413);
+`;
+
+const YourLibrary: NextPage<IProps> = ({ profileData }) => {
+  const { toggleSidebar, openSidebar } = toggleSidebarHook();
+
+  return (
+    <StyledContainer opensidebar={openSidebar}>
+      <Sidebar openSidebar={openSidebar} />
+      <StyledRightSideContainer opensidebar={openSidebar}>
+        <Navbar
+          openSidebar={openSidebar}
+          toggleSidebar={toggleSidebar}
+          profileData={profileData}
+          libraryMenu={true}
+        />
+        <Library />
+        <Global
+          styles={css`
+            html,
+            body {
+              padding: 0;
+              margin: 0 !important;
+              font-family: "arial", sans-serif;
+            }
+            a {
+              text-decoration: none;
+              cursor: pointer;
+              color: #b3b3b3;
+              &:hover {
+                color: #fff;
+              }
+            }
+          `}
+        />
+      </StyledRightSideContainer>
+    </StyledContainer>
+  );
+};
+
+YourLibrary.getInitialProps = async ({ reduxStore }: IReduxStoreProps) => {
+  const store = reduxStore.getState();
+
+  return {
+    profileData: store.profile,
+  };
+};
+export default withRedux(YourLibrary);
