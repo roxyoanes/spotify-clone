22910eda68e7bd04f2732c4910a290567ecc74e1
roxyoanes <roxioanes11@gmail.com>
2020-08-25 17:35:04 +0200

    Add types for saved albums and followed artists

--- src/types.ts
+++ src/types.ts
@@ -392,3 +392,176 @@ export interface IGetAlbum {
   type: string;
   uri: string;
 }
+
+export interface IGetUserAlbums {
+  href: string;
+  items: [
+    {
+      added_at: string;
+      album: {
+        album_type: string;
+        artists: [
+          {
+            external_urls: {
+              spotify: string;
+            };
+            href: string;
+            id: string;
+            name: string;
+            type: string;
+            uri: string;
+          }
+        ];
+        available_markets: string[];
+        copyrights: [
+          {
+            text: string;
+            type: string;
+          },
+          {
+            text: string;
+            type: string;
+          }
+        ];
+        external_ids: {
+          upc: string;
+        };
+        external_urls: {
+          spotify: string;
+        };
+        genres: [];
+        href: string;
+        id: string;
+        images: IImage[];
+        name: string;
+        popularity: number;
+        release_date: string;
+        release_date_precision: string;
+        tracks: {
+          href: string;
+          items: [
+            {
+              artists: [
+                {
+                  external_urls: {
+                    spotify: string;
+                  };
+                  href: string;
+                  id: string;
+                  name: string;
+                  type: string;
+                  uri: string;
+                },
+                {
+                  external_urls: {
+                    spotify: string;
+                  };
+                  href: string;
+                  id: string;
+                  name: string;
+                  type: string;
+                  uri: string;
+                },
+                {
+                  external_urls: {
+                    spotify: string;
+                  };
+                  href: string;
+                  id: string;
+                  name: string;
+                  type: string;
+                  uri: string;
+                }
+              ];
+              available_markets: string[];
+              disc_number: number;
+              duration_ms: number;
+              explicit: boolean;
+              external_urls: {
+                spotify: string;
+              };
+              href: string;
+              id: string;
+              name: string;
+              preview_url: string;
+              track_number: number;
+              type: string;
+              uri: string;
+            },
+            {
+              artists: [
+                {
+                  external_urls: {
+                    spotify: string;
+                  };
+                  href: string;
+                  id: string;
+                  name: string;
+                  type: string;
+                  uri: string;
+                }
+              ];
+              available_markets: string[];
+              disc_number: number;
+              duration_ms: number;
+              explicit: boolean;
+              external_urls: {
+                spotify: string;
+              };
+              href: string;
+              id: string;
+              name: string;
+              preview_url: string;
+              track_number: number;
+              type: string;
+              uri: string;
+            }
+          ];
+          limit: number;
+          next: null;
+          offset: number;
+          previous: null;
+          total: number;
+        };
+        type: string;
+        uri: string;
+      };
+    }
+  ];
+  limit: number;
+  next: string;
+  offset: number;
+  previous: null;
+  total: number;
+}
+
+export interface IFollowedArtists {
+  artists: {
+    items: [
+      {
+        external_urls: {
+          spotify: string;
+        };
+        followers: {
+          href: null;
+          total: number;
+        };
+        genres: [];
+        href: string;
+        id: string;
+        images: IImage[];
+        name: string;
+        popularity: number;
+        type: string;
+        uri: string;
+      }
+    ];
+    next: string;
+    total: number;
+    cursors: {
+      after: string;
+    };
+    limit: number;
+    href: number;
+  };
+}
