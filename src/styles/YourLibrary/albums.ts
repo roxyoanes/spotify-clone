0ad93e430f7534d2c47253cbd95f918b2d44f270
roxyoanes <roxioanes11@gmail.com>
2020-08-25 12:43:12 +0200

    Add styling for albums page

---
+++ src/styles/YourLibrary/albums.ts
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+
+const StyledAlbums = styled.div`
+  color: white;
+  background-color: #131413;
+
+  .albums-wrapper {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    justify-content: center;
+    grid-gap: 30px;
+    margin: 0 20px;
+  }
+  .card-title {
+    margin: 20px 30px;
+    font-size: 24px;
+  }
+  .card {
+    background-color: #212121;
+    border-radius: 15px;
+    padding: 15px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
+  }
+  .album-pic {
+    height: 125px;
+  }
+  .text-card {
+    margin-top: 14px;
+  }
+  .name {
+    font-size: 14px;
+    margin: 0;
+  }
+  .info-artist {
+    color: #b3b3b3;
+    font-size: 14px;
+    margin: 10px 0;
+  }
+`;
+
+export default StyledAlbums;
