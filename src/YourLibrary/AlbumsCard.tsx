3a13f6f496b324e936cb121cb09302c259ee9a5d
roxyoanes <roxioanes11@gmail.com>
2020-08-25 12:43:40 +0200

    Add albums data to component

--- src/YourLibrary/AlbumsCard.tsx
+++ src/YourLibrary/AlbumsCard.tsx
@@ -1,24 +1,27 @@
 import { NextPage } from "next";
 
-import StyledAlbum from "../styles/album";
+import StyledAlbums from "../styles/YourLibrary/albums";
 
-interface IProps {}
+interface IProps {
+  savedAlbums: any;
+}
 
-const AlbumsCard: NextPage<IProps> = () => {
+const AlbumsCard: NextPage<IProps> = ({ savedAlbums }) => {
+  console.log("dd", savedAlbums);
   return (
-    <StyledAlbum>
+    <StyledAlbums>
       <div className="container">
-        <div className="albums-main">
-          <h1>Albums</h1>
-          {/*  {savedAlbums.items.map((savedAlbum) => (
-            <div key={savedAlbum.id}>
+        <h1 className="card-title">Albums</h1>
+        <div className="albums-wrapper">
+          {savedAlbums.items.map((savedAlbum) => (
+            <div className="card" key={savedAlbum.id}>
               <img
                 className="album-pic"
                 src={savedAlbum.album.images[0].url}
                 alt="album-pic"
               />
               <div className="text-card">
-                <h1 className="name">{savedAlbum.name}</h1>
+                <h6 className="name">{savedAlbum.album.name}</h6>
 
                 {savedAlbum.album.artists.map((artist) => (
                   <p className="info-artist" key={artist.id}>
@@ -27,10 +30,10 @@ const AlbumsCard: NextPage<IProps> = () => {
                 ))}
               </div>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
-    </StyledAlbum>
+    </StyledAlbums>
   );
 };
 
