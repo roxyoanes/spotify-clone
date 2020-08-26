700744f1908bbe6fb0cb2f99f624858b3907474b
roxyoanes <roxioanes11@gmail.com>
2020-08-26 16:22:19 +0200

    Add api data in artists card

--- src/YourLibrary/ArtistsCard.tsx
+++ src/YourLibrary/ArtistsCard.tsx
@@ -1,22 +1,38 @@
 import { NextPage } from "next";
 
-import StyledAlbum from "../styles/album";
 import { IFollowedArtists } from "../types";
+import StyledFollowedArtists from "../styles/YourLibrary/artists";
 
 interface IProps {
   followedArtists: IFollowedArtists;
 }
 
 const ArtistsCard: NextPage<IProps> = ({ followedArtists }) => {
-  console.log("sss", followedArtists);
   return (
-    <StyledAlbum>
+    <StyledFollowedArtists>
       <div className="container">
         <div className="artists-main">
-          <h1>Artists</h1>
+          <h1 className="card-title">Artists</h1>
+          <div className="artists-wrapper">
+            {followedArtists.artists.items.map((followedArtist) => (
+              <div className="card" key={followedArtist.id}>
+                <img
+                  className="artist-pic"
+                  src={followedArtist.images[0].url}
+                  alt="artist-pic"
+                />
+                <div className="text-card">
+                  <h6 className="name">{followedArtist.name}</h6>
+                  <p className="info-artist" key={followedArtist.id}>
+                    {followedArtist.type}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </StyledAlbum>
+    </StyledFollowedArtists>
   );
 };
 
