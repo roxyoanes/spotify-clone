adaa88ebc016013ca6b7444735cf7728249b4aec
roxyoanes <roxioanes11@gmail.com>
2020-08-31 17:30:35 +0200

    Convert milliseconds to minutes and seconds

--- src/YourLibrary/LikedTracksCard.tsx
+++ src/YourLibrary/LikedTracksCard.tsx
@@ -8,8 +8,14 @@ interface IProps {
   savedTracks: any;
 }
 
+const convertMilliseconds = (milliseconds) => {
+  const min = Math.floor((milliseconds / 1000 / 60) << 0);
+  const sec = Math.floor((milliseconds / 1000) % 60);
+  const res = min + ":" + sec;
+  return res;
+};
+
 const LikedTracksCard: NextPage<IProps> = ({ profileData, savedTracks }) => {
-  console.log("dd", savedTracks);
   return (
     <StyledLikedTracks>
       <div className="container">
@@ -29,25 +35,59 @@ const LikedTracksCard: NextPage<IProps> = ({ profileData, savedTracks }) => {
               <p className="user-name">{profileData.display_name}</p>
             </div>
           </div>
-          <div className="playlist-container"></div>
         </div>
-        {/*  <div className="artists-wrapper">
-            {followedArtists.artists.items.map((followedArtist) => (
-              <div className="card" key={followedArtist.id}>
+        <div className="button-container">
+          <button className="play-btn">
+            <img
+              className="play-img"
+              src="/play-solid.svg"
+              alt="play-icon"
+            ></img>
+          </button>
+        </div>
+        <div className="tracks-header">
+          <h5 className="header-item"># TITLE</h5>
+          <h5 className="header-item">ALBUM</h5>
+          <h5 className="header-item">DATE ADDED</h5>
+          <img
+            className="clock-icon header-item"
+            src="/clock-regular.svg"
+            alt="clock-icon"
+          />
+        </div>
+        <div className="tracks-wrapper">
+          {savedTracks.items.map((savedTrack) => (
+            <div className="card" key={savedTrack.id}>
+              <div className="card-title">
                 <img
-                  className="artist-pic"
-                  src={followedArtist.images[0].url}
-                  alt="artist-pic"
+                  className="tracks-pic"
+                  src={savedTrack.track.album.images[0].url}
+                  alt="tracks-pic"
                 />
                 <div className="text-card">
-                  <h6 className="name">{followedArtist.name}</h6>
-                  <p className="info-artist" key={followedArtist.id}>
-                    {followedArtist.type}
+                  <p className="song-name" key={savedTrack.id}>
+                    {savedTrack.track.name}
                   </p>
+                  {savedTrack.track.album.artists.map((artist) => (
+                    <p className="info-artist" key={artist.id}>
+                      {artist.name}
+                    </p>
+                  ))}
                 </div>
               </div>
-            ))}
-          </div> */}
+
+              <p className="info-artist" key={savedTrack.track.album.id}>
+                {savedTrack.track.album.name}
+              </p>
+              <p className="info-artist" key={savedTrack.track.album.id}>
+                {savedTrack.added_at}
+              </p>
+              <p className="info-artist" key={savedTrack.track.album.id}>
+                {convertMilliseconds(savedTrack.track.duration_ms)}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </StyledLikedTracks>
   );
--- src/styles/YourLibrary/likedTracks.ts
+++ src/styles/YourLibrary/likedTracks.ts
@@ -4,15 +4,18 @@ const StyledLikedTracks = styled.div`
   color: white;
   background-color: #131413;
 
+  .container {
+    padding: 40px;
+  }
   .wrapper-header {
     display: flex;
     align-items: center;
-    padding: 40px;
   }
   .icon-background {
     background: linear-gradient(135deg, #450af5, #c4efd9);
     padding: 55px;
   }
+
   .icon {
     height: 80px;
   }
@@ -21,18 +24,47 @@ const StyledLikedTracks = styled.div`
     margin: 10px 0;
   }
   .card {
-    background-color: #212121;
     border-radius: 15px;
     padding: 15px 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     cursor: pointer;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    align-items: center;
+  }
+  .card-title {
+    display: flex;
+    align-items: center;
   }
   .user-pic {
     height: 25px;
     border-radius: 50%;
     margin-right: 10px;
+  }
+  .button-container {
+    display: flex;
+    align-items: center;
+    padding: 20px 0;
+  }
+  .tracks-header {
+    color: #b3b3b3;
+    border-bottom: 1px solid hsla(0, 0%, 100%, 0.1);
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    align-items: center;
+  }
+
+  .clock-icon {
+    height: 20px;
+  }
+  .play-btn {
+    padding: 15px 20px;
+    background-color: #1db954;
+    border: none;
+    border-radius: 50%;
+    margin: 0 10px;
+  }
+  .play-img {
+    width: 20px;
   }
   .text-card {
     padding: 20px 40px;
@@ -49,6 +81,14 @@ const StyledLikedTracks = styled.div`
     color: #b3b3b3;
     font-size: 14px;
     margin: 10px 0;
+  }
+  .song-name {
+    color: white;
+    font-size: 16px;
+    margin: 0;
+  }
+  .tracks-pic {
+    height: 60px;
   }
 `;
 
