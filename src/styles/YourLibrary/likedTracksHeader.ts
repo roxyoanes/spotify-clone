043c129d1a214848d456fff64d86ef5afb601c07
roxyoanes <roxioanes11@gmail.com>
2020-09-02 11:54:58 +0200

    Create new component

---
+++ src/styles/YourLibrary/likedTracksHeader.ts
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+
+const StyledLikedTracksHeader = styled.div`
+  color: white;
+
+  .wrapper-header {
+    display: flex;
+    align-items: center;
+    padding: 40px 60px;
+  }
+  .icon-background {
+    background: linear-gradient(135deg, #450af5, #c4efd9);
+    padding: 55px;
+  }
+  .icon {
+    height: 80px;
+  }
+  .text-card {
+    padding: 20px 40px;
+  }
+  .user-info-container {
+    display: flex;
+    align-items: center;
+  }
+
+  .card-title {
+    font-size: 65px;
+    margin: 10px 0;
+    display: flex;
+    align-items: center;
+  }
+  .user-pic {
+    height: 25px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+  .user-name {
+    font-weight: 700;
+    font-size: 14px;
+  }
+`;
+
+export default StyledLikedTracksHeader;
