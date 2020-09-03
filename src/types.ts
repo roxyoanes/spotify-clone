e7d41a3a3dd0c288bc4dc5504980ff05c9b1f423
roxyoanes <roxioanes11@gmail.com>
2020-09-03 16:08:25 +0200

    Add types for savedTracks

--- src/types.ts
+++ src/types.ts
@@ -565,3 +565,74 @@ export interface IFollowedArtists {
     href: number;
   };
 }
+
+export interface ISavedTracks {
+  href: string;
+  items: [
+    {
+      added_at: string;
+      track: {
+        album: {
+          album_type: string;
+          artists: [
+            {
+              external_urls: {
+                spotify: string;
+              };
+              href: string;
+              id: string;
+              name: string;
+              type: string;
+              uri: string;
+            }
+          ];
+          available_markets: string[];
+          external_urls: {
+            spotify: string;
+          };
+          href: string;
+          id: string;
+          images: IImage[];
+          name: string;
+          type: string;
+          uri: string;
+        };
+        artists: [
+          {
+            external_urls: {
+              spotify: string;
+            };
+            href: string;
+            id: string;
+            name: string;
+            type: string;
+            uri: string;
+          }
+        ];
+        available_markets: string[];
+        disc_number: number;
+        duration_ms: number;
+        explicit: false;
+        external_ids: {
+          isrc: string;
+        };
+        external_urls: {
+          spotify: string;
+        };
+        href: string;
+        id: string;
+        name: string;
+        popularity: number;
+        preview_url: string;
+        track_number: number;
+        type: string;
+        uri: string;
+      };
+    }
+  ];
+  limit: number;
+  next: string;
+  offset: number;
+  previous: null;
+  total: number;
+}
