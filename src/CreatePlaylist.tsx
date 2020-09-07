6f14cac4e9f7996eabc876bc8d6bd7c258bddefb
roxyoanes <roxioanes11@gmail.com>
2020-09-07 16:21:04 +0200

    Add input and buttons

--- src/CreatePlaylist.tsx
+++ src/CreatePlaylist.tsx
@@ -9,12 +9,21 @@ const CreatePlaylist: FC<IProps> = ({ toggleModal }) => {
   return (
     <StyledCreatePlaylist>
       <div className="container">
-        <div className="modal">
+        <div className="s">
           <button type="button" className="btn" onClick={toggleModal}>
             X
           </button>
-          <p>hey</p>
+          <p className="title">Create new playlist</p>
         </div>
+
+        <div className="playlist-input">
+          <h4 className="input-title">Playlist Name</h4>
+          <input placeholder="New Playlist" className="input"></input>
+        </div>
+      </div>
+      <div className="buttons-container">
+        <button className="input-btn cancel-btn">CANCEL</button>
+        <button className="input-btn create-btn">CREATE</button>
       </div>
     </StyledCreatePlaylist>
   );
