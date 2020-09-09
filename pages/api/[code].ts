17b9da4991575a882827ee8556f1811758c64e3a
roxyoanes <roxioanes11@gmail.com>
2020-09-09 17:06:37 +0200

    Move code to new file

--- pages/api/[code].ts
+++ pages/api/[code].ts
@@ -1,24 +1,15 @@
-import { spotifyApi } from "../../src/server/spotifyApi";
+import { getAuthorizationCode } from "../../src/server/spotifyApi";
 
 export default (req, res) => {
   const {
     query: { code },
   } = req;
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then((data) => {
-      console.log("The token expires in " + data.body["expires_in"]);
-      console.log("The access token is " + data.body["access_token"]);
-      console.log("The refresh token is " + data.body["refresh_token"]);
-
-      // Set the access token on the API object to use it in later calls
-      spotifyApi.setAccessToken(data.body["access_token"]);
-      spotifyApi.setRefreshToken(data.body["refresh_token"]);
-
+  getAuthorizationCode(code)
+    .then(({ access_token, refresh_token }) => {
       res.json({
-        access_token: data.body["access_token"],
-        refresh_token: data.body["refresh_token"],
+        access_token,
+        refresh_token,
       });
     })
     .catch((err) => {
--- src/server/spotifyApi.ts
+++ src/server/spotifyApi.ts
@@ -7,16 +7,41 @@ export const scopes = [
     "user-read-recently-played",
     "user-read-private",
     "user-follow-read",
+    "playlist-modify-private",
   ],
   redirectUri = "http://localhost:3000/login",
   clientId = process.env.CLIENT_ID,
   clientSecret = process.env.CLIENT_SECRET;
 
 // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-export const spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   redirectUri: redirectUri,
   clientId: clientId,
   clientSecret: clientSecret,
 });
 
 export const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+
+export const getAuthorizationCode = (code: string) => {
+  return spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      console.log("The token expires in " + data.body["expires_in"]);
+      console.log("The access token is " + data.body["access_token"]);
+      console.log("The refresh token is " + data.body["refresh_token"]);
+
+      // Set the access token on the API object to use it in later calls
+      spotifyApi.setAccessToken(data.body["access_token"]);
+      spotifyApi.setRefreshToken(data.body["refresh_token"]);
+
+      return {
+        access_token: data.body["access_token"],
+        refresh_token: data.body["refresh_token"],
+      };
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export default spotifyApi;
