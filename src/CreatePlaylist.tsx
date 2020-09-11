d527403ad1c9097cb8ef7b6e5c4e5ec6c19ca1d8
roxyoanes <roxioanes11@gmail.com>
2020-09-11 11:28:01 +0200

    Add functionality for create button

--- src/CreatePlaylist.tsx
+++ src/CreatePlaylist.tsx
@@ -1,29 +1,48 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import StyledCreatePlaylist from "./styles/createPlaylist";
+import React from "react";
 
 interface IProps {
   toggleModal: () => void;
+  userId: string;
 }
-const CreatePlaylist: FC<IProps> = ({ toggleModal }) => {
+const CreatePlaylist: FC<IProps> = ({ toggleModal, userId }) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleCreatePlaylist = () => {
+    fetch(`/api/playlist/create-playlist/${value}?userId=${userId}`);
+  };
+
   return (
     <StyledCreatePlaylist>
-      <div className="container">
-        <div className="s">
-          <button type="button" className="btn" onClick={toggleModal}>
-            X
-          </button>
-          <p className="title">Create new playlist</p>
-        </div>
+      <div className="header">
+        <button type="button" className="btn" onClick={toggleModal}>
+          X
+        </button>
+        <p className="title">Create new playlist</p>
+      </div>
 
-        <div className="playlist-input">
-          <h4 className="input-title">Playlist Name</h4>
-          <input placeholder="New Playlist" className="input"></input>
-        </div>
+      <div className="playlist-input">
+        <h4 className="input-title">Playlist Name</h4>
+        <input
+          placeholder="New Playlist"
+          className="input"
+          value={value}
+          onChange={handleChange}
+        />
       </div>
       <div className="buttons-container">
-        <button className="input-btn cancel-btn">CANCEL</button>
-        <button className="input-btn create-btn">CREATE</button>
+        <button className="input-btn cancel-btn" onClick={toggleModal}>
+          CANCEL
+        </button>
+        <button className="input-btn create-btn" onClick={handleCreatePlaylist}>
+          CREATE
+        </button>
       </div>
     </StyledCreatePlaylist>
   );
