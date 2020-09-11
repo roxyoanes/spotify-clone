05fe31e7b787e48f9714185586317eee494f8faf
roxyoanes <roxioanes11@gmail.com>
2020-09-11 11:33:48 +0200

    Create backend route

---
+++ pages/api/getProfileData.ts
@@ -0,0 +1,35 @@
+import { withIronSession } from "next-iron-session";
+
+import { initializeSpotifyApi } from "../../src/server/spotifyApi";
+
+const handler = async (req, res) => {
+  const token = req.session.get("token");
+
+  const isLoggedIn = !!token;
+
+  if (isLoggedIn) {
+    const spotifyApi = initializeSpotifyApi(token);
+
+    const profileData = await spotifyApi.getMe();
+
+    res.json({
+      profileData: profileData.body,
+    });
+    try {
+    } catch (err) {
+      res.send(err);
+    }
+  }
+  res.json({
+    isLoggedIn,
+  });
+};
+
+export default withIronSession(handler, {
+  password: "complex_password_at_least_32_characters_long",
+  // if your localhost is served on http:// then disable the secure flag
+  cookieName: "token",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
