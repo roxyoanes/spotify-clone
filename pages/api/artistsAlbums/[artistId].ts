be5f1adf93ec9c352e4aaf6c33cd3c0b394a70e3
roxyoanes <roxioanes11@gmail.com>
2020-10-19 17:00:15 +0200

    getartistsAlbum api call

---
+++ pages/api/artistsAlbums/[artistId].ts
@@ -0,0 +1,39 @@
+import { initializeSpotifyApi } from "../../../src/server/spotifyApi";
+import { withIronSession } from "next-iron-session";
+
+const handler = async (req, res) => {
+  const token = req.session.get("token");
+  const {
+    query: { artistId },
+  } = req;
+
+  const isLoggedIn = !!token;
+
+  if (isLoggedIn) {
+    const spotifyApi = initializeSpotifyApi(token);
+
+    try {
+      const artistAlbumData = await spotifyApi.getArtistAlbums(artistId, {
+        limit: 10,
+      });
+
+      res.json({
+        artistAlbumData: artistAlbumData.body,
+      });
+    } catch (err) {
+      res.send(err);
+    }
+  }
+  res.json({
+    isLoggedIn,
+  });
+};
+
+export default withIronSession(handler, {
+  password: "complex_password_at_least_32_characters_long",
+  // if your localhost is served on http:// then disable the secure flag
+  cookieName: "token",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
