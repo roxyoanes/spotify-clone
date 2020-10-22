0c16d60c6e050aa254879d31d336a08124e3fee5
roxyoanes <roxioanes11@gmail.com>
2020-10-22 17:36:01 +0200

    Pass data to component

--- src/YourLibrary/ArtistInfo.tsx
+++ src/YourLibrary/ArtistInfo.tsx
@@ -1,18 +1,27 @@
 import { FC } from "react";
+import StyledArtistInfo from "../styles/YourLibrary/artistInfo";
 
 interface IProps {
-  artistData: any;
+  artistInfo: any; //change this
 }
 
-const ArtistInfo: FC<IProps> = ({ artistData }) => {
-  console.log("ff", artistData);
+const ArtistInfo: FC<IProps> = ({ artistInfo }) => {
+  console.log("ff", artistInfo);
 
   return (
-    <div className="card">
-      <div className="artist-header">
-        <p>Verified Artist</p>
+    <StyledArtistInfo>
+      <div className="card">
+        <div className="artist-header">
+          <img
+            src={artistInfo.images[0].url}
+            className="artist-pic"
+            alt="artist-picture"
+          />
+          <p>Verified Artist</p>
+          <h4>{artistInfo.name}</h4>
+        </div>
       </div>
-    </div>
+    </StyledArtistInfo>
   );
 };
 
