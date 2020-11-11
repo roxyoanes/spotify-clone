2a988fe52861700803bd6c3e30f3bc59ad6c533a
roxyoanes <roxioanes11@gmail.com>
2020-11-11 11:31:31 +0100

    Add style artist info

--- src/styles/YourLibrary/artistInfo.ts
+++ src/styles/YourLibrary/artistInfo.ts
@@ -2,6 +2,29 @@ import styled from "@emotion/styled";
 
 const StyledArtistInfo = styled.div`
   color: white;
+  padding: 20px 30px;
+  margin: 0 40px;
+
+  p {
+    font-size: 20px;
+    margin: 0;
+  }
+
+  .img-artist {
+    height: 280px;
+  }
+  .artist-header {
+    display: flex;
+    align-items: end;
+    padding: 40px 30px;
+  }
+  .text-card {
+    margin-left: 40px;
+  }
+  .artist-name {
+    font-size: 54px;
+    margin: 10px 0;
+  }
 `;
 
 export default StyledArtistInfo;
