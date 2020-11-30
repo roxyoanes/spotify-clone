e1c85b3765b2f53f6a4b9aaf85ea8e52ecc3c366
roxyoanes <roxioanes11@gmail.com>
2020-11-30 12:18:04 +0100

    Add style for the card

--- src/styles/YourLibrary/artistAlbums.ts
+++ src/styles/YourLibrary/artistAlbums.ts
@@ -1,6 +1,15 @@
 import styled from "@emotion/styled";
 
 const StyledArtistAlbums = styled.div`
+  h3 {
+    font-size: 16px;
+  }
+  h5 {
+    color: #b3b3b3;
+    font-weight: 200;
+    font-size: 14px;
+    margin: 5px 0;
+  }
   .title {
     padding: 20px 0;
     font-size: 26px;
@@ -15,9 +24,27 @@ const StyledArtistAlbums = styled.div`
     grid-template-columns: repeat(4, 1fr);
     justify-items: center;
   }
+  .album-card {
+    background-color: #212121;
+    border-radius: 15px;
+    padding: 15px;
+    display: grid;
+    cursor: pointer;
+  }
 
   .album-img {
-    height: 180px;
+    height: 200px;
+  }
+  .card-text {
+    display: flex;
+  }
+  .type::before {
+    content: "•";
+    margin: 4px;
+    color: #b3b3b3;
+  }
+  .type {
+    display: flex;
   }
 `;
 
