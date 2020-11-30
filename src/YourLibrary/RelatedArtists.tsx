ce4178cd2e4dd609a7fb470cda62a16ef107697d
roxyoanes <roxioanes11@gmail.com>
2020-11-30 12:18:46 +0100

    Add component for related artists

---
+++ src/YourLibrary/RelatedArtists.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import StyledRelatedArtists from "../styles/YourLibrary/relatedArtists";
+
+interface IProps {
+  relatedArtists: any; //change this
+}
+
+const RelatedArtists: FC<IProps> = ({ relatedArtists }) => {
+  console.log("sbbb", relatedArtists);
+  return (
+    <StyledRelatedArtists>
+      <div className="related-artists-container">
+        <p className="title">Fans also like</p>
+        <div className="cards-container">
+          {relatedArtists.artists.slice(0, 4).map((artist) => (
+            <div className="related-artist-card" key={artist.id}>
+              <img
+                src={artist.images[0].url}
+                alt="artist-pic"
+                className="artist-img"
+              />
+              <h3 className="name">{artist.name}</h3>
+              <h5 className="type">{artist.type}</h5>
+            </div>
+          ))}
+        </div>
+      </div>
+    </StyledRelatedArtists>
+  );
+};
+
+export default RelatedArtists;
