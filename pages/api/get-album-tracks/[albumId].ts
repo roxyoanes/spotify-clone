e202a2668651340dca56db9bf77b688ae941391a
roxyoanes <roxioanes11@gmail.com>
2020-12-01 12:39:55 +0100

    Add api call

---
+++ pages/api/get-album-tracks/[albumId].ts
@@ -0,0 +1,37 @@
+import { initializeSpotifyApi } from "../../../src/server/spotifyApi";
+import { withIronSession } from "next-iron-session";
+
+const handler = async (req, res) => {
+  const token = req.session.get("token");
+  const {
+    query: { albumId },
+  } = req;
+
+  const isLoggedIn = !!token;
+
+  if (isLoggedIn) {
+    const spotifyApi = initializeSpotifyApi(token);
+
+    try {
+      const albumTracksData = await spotifyApi.getAlbumTracks(albumId);
+
+      res.json({
+        albumTracksData: albumTracksData.body,
+      });
+    } catch (err) {
+      res.send(err);
+    }
+  }
+  res.json({
+    isLoggedIn,
+  });
+};
+
+export default withIronSession(handler, {
+  password: "complex_password_at_least_32_characters_long",
+  // if your localhost is served on http:// then disable the secure flag
+  cookieName: "token",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
