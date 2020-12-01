450f8ace17bedc14364a568c1fa903e5dcf64716
roxyoanes <roxioanes11@gmail.com>
2020-12-01 12:40:28 +0100

    Add styled component

---
+++ src/styles/YourLibrary/albumTracks.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+const StyledAlbumTracks = styled.div`
+  h3 {
+    font-size: 16px;
+  }
+  h5 {
+    color: #b3b3b3;
+    font-weight: 200;
+    font-size: 14px;
+    margin: 5px 0;
+  }
+`;
+
+export default StyledAlbumTracks;
